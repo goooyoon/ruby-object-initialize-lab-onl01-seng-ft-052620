facd14444498688c12ed90e231580a5f8e0f9f6d
Grace Kim <goooyoon@gmail.com>
2020-06-02 22:27:09 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,18 +1,5 @@
-
-
-
-
-
-	.	class Dog
-	.	  def initialize(breed)
-	.	    @breed = breed
-	.	  end
-	.	 
-	.	  def breed=(breed)
-	.	    @breed = breed
-	.	  end
-	.	 
-	.	  def breed
-	.	    @breed
-	.	  end
-	.	end
+class Person
+  def initialize(name)
+    @name = name
+  end
+end
